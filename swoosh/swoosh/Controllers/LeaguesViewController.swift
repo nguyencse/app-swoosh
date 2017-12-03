e019cc9916cd47075b7e13d855fc717a7dc1279e
Nguyen Y Nguyen <nguyencse.ninhhoa@gmail.com>
2017-12-03 13:35:35 +0700

    completed section 4 - lecture 44 - refactoring in Xcode 9

---
+++ swoosh/swoosh/Controllers/LeaguesViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LeaguesViewController.swift
+//  swoosh
+//
+//  Created by Nguyen Y Nguyen on 12/3/17.
+//  Copyright © 2017 Nguyen Y Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class LeaguesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
